4f43e6dcc52e413cf6f692b33ae5418b89874a36
saraswatikeshatwar <69385313+saraswatikeshatwar@users.noreply.github.com>
2020-08-23 09:51:48 +0530

    Update Day28-RegExPatterns.cs

--- Day28-RegExPatterns.cs
+++ Day28-RegExPatterns.cs
@@ -1,56 +1,44 @@
-#include <bits/stdc++.h>
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-using namespace std;
-
-vector<string> split_string(string);
+class Solution {
 
 
 
-int main()
-{
-    int N;
-    cin >> N;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    static void Main(string[] args) {
+         int N = Convert.ToInt32(Console.ReadLine());
+            var list = new List<string>();
 
-    for (int N_itr = 0; N_itr < N; N_itr++) {
-        string firstNameEmailID_temp;
-        getline(cin, firstNameEmailID_temp);
+            for (int NItr = 0; NItr < N; NItr++)
+            {
+                string[] firstNameEmailID = Console.ReadLine().Split(' ');
 
-        vector<string> firstNameEmailID = split_string(firstNameEmailID_temp);
+                string firstName = firstNameEmailID[0];
 
-        string firstName = firstNameEmailID[0];
+                string emailID = firstNameEmailID[1];
 
-        string emailID = firstNameEmailID[1];
+                if (Regex.IsMatch(emailID, @".+@gmail\.com$"))
+                {
+                    list.Add(firstName);
+                }
+
+                list.Sort();
+            
+            }
+            foreach (var name in list)
+                {
+                    Console.WriteLine(name);
+                }
     }
-
-    return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
 }
