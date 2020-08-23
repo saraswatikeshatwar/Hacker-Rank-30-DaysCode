68b7912d35651d8181b4b027788cab3533359fe7
saraswatikeshatwar <69385313+saraswatikeshatwar@users.noreply.github.com>
2020-08-23 09:22:49 +0530

    StringToInteger.cs

---
+++ Day16-Exceptions.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        string S = Console.ReadLine();
+        try
+            {
+                Console.WriteLine(Convert.ToInt32(S));
+            }
+            catch 
+            {
+                Console.WriteLine("Bad String");
+            }
+        
+    }
+}
