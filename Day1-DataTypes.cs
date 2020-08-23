5f2f54b20d1426a53f52787b8b0786d2eb8fc9bd
saraswatikeshatwar <69385313+saraswatikeshatwar@users.noreply.github.com>
2020-08-23 08:48:44 +0530

    Create Day1-DataTypes.cs

---
+++ Day1-DataTypes.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+
+        
+        // Declare second integer, double, and String variables.
+        
+        // Read and save an integer, double, and String to your variables.
+        
+        // Print the sum of both integer variables on a new line.
+        
+        // Print the sum of the double variables on a new line.
+        
+        // Concatenate and print the String variables on a new line
+        // The 's' variable above should be printed first.
+        
+            int a =Convert.ToInt32(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            string c = Console.ReadLine();
+
+            Console.WriteLine(i + a);
+            Console.WriteLine((d + b).ToString("F1"));
+            Console.WriteLine(s + c);
+
+
+    }
+}
